b3445404e1c3c779568f0b8420e9c68029413062
ddrsoul <107053842+ddrsoul@users.noreply.github.com>
2024-08-30 14:59:58 +0300

    Update TheXtech.sh

--- TheXtech.sh
+++ TheXtech.sh
@@ -23,7 +23,7 @@ GAMEDIR=/$directory/ports/thextech
 export TEXTINPUTINTERACTIVE="Y"
 export TEXTINPUTNOAUTOCAPITALS="Y"
 
-export LD_LIBRARY_PATH="$GAMEDIR/libs":LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 $ESUDO chmod 666 /dev/uinput
